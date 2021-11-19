b7c320a9324f0dfdc782e253bc6732c2c6ffd4e2
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-19 22:34:07 +0300

    change file

--- IT-KAMASUTRA/tempyarn/my-app/src/App.tsx
+++ IT-KAMASUTRA/tempyarn/my-app/src/App.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 
-function hello() {
+/*function hello() {
     debugger
     alert("Hello IT-Incubator")
-}
+}*/
+
 //hello();
+
 //function declaration
 function App() {
     // полезное что-то
-debugger
     // обязана вернуть JSX
+    console.log("App rendering")
     return (
         <div>
-            This is APP component
-            <Rating />
-            <Accordion />
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+
         </div>
     );
 }
 
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return <>This is APP component</>
+
+}
 
 function Rating() {
-    debugger
+    console.log("Rating rendering")
     return (
         <div>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </div>
 
     )
@@ -39,20 +48,36 @@ function Rating() {
 }
 
 function Accordion() {
-    debugger
+    console.log("Accordion rendering")
+    return <div>
+        <AccordionTitle/>
+        <AccordionBody/>
+    </div>
+
+}
+
+function Star() {
+    console.log("Star rendering")
+    return <div>star</div>
+}
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
     return <div>
         <h3>Меню</h3>
+    </div>
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return <div>
         <ul>
             <li>1</li>
             <li>2</li>
             <li>3</li>
         </ul>
     </div>
-
 }
 
-function Star() {
-    return  <div>star</div>
-}
 export default App;
 
