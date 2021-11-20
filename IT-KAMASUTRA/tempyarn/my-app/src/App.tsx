96e442998b1cedb7660b07043d6f034cb739d656
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-20 13:57:56 +0300

    added new files

--- IT-KAMASUTRA/tempyarn/my-app/src/App.tsx
+++ IT-KAMASUTRA/tempyarn/my-app/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 
 /*function hello() {
@@ -30,53 +32,6 @@ function AppTitle() {
     console.log("AppTitle rendering")
     return <>This is APP component</>
 
-}
-
-function Rating() {
-    console.log("Rating rendering")
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-
-    )
-
-}
-
-function Accordion() {
-    console.log("Accordion rendering")
-    return <div>
-        <AccordionTitle/>
-        <AccordionBody/>
-    </div>
-
-}
-
-function Star() {
-    console.log("Star rendering")
-    return <div>star</div>
-}
-
-function AccordionTitle() {
-    console.log("AccordionTitle rendering")
-    return <div>
-        <h3>Меню</h3>
-    </div>
-}
-
-function AccordionBody() {
-    console.log("AccordionBody rendering")
-    return <div>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </div>
 }
 
 export default App;
---
+++ IT-KAMASUTRA/tempyarn/my-app/src/components/Rating/Rating.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Rating() {
+    console.log("Rating rendering")
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+
+    )
+
+}
+
+function Star() {
+    console.log("Star rendering")
+    return <div>star</div>
+}
