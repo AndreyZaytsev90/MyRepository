dfecdcb7039057e33654349992c8b991da7eb74a
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-20 17:42:51 +0300

    added props

--- IT-KAMASUTRA/tempyarn/my-app/src/components/Rating/Rating.tsx
+++ IT-KAMASUTRA/tempyarn/my-app/src/components/Rating/Rating.tsx
@@ -10,12 +10,16 @@ export function Rating() {
             <Star selected={false}/>
             <Star selected={false}/>
         </div>
-
     )
-
 }
 
-function Star() {
+function Star(props: any) {
     console.log("Star rendering")
-    return <span><b>star </b></span>
+    if (props.selected === true) {
+        return <span><b>star </b></span>
+    } else {
+        return <span>star </span>
+    }
+
+
 }
