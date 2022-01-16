de0394463ae616f71c0df67158f21dac6931ed16
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-01-16 17:11:12 +0300

    added uncontrolled components

---
+++ IT-KAMASUTRA/tempyarn/my-app/src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    //collapsed: boolean
+}
+
+function UncontrolledAccordion(props: AccordionPropsType) {
+    console.log("Accordion rendering")
+
+
+    let [collapsed, setCollapsed] = useState(true)
+
+    return <div>
+        <AccordionTitle title={props.titleValue}/> <button onClick={() => { collapsed ? setCollapsed(false) : setCollapsed(true)}}>Down</button>
+        {!collapsed && <AccordionBody/>}
+    </div>
+}
+
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log("AccordionTitle rendering")
+    return <h3>--- {props.title} ---</h3>
+}
+
+function AccordionBody(props: any) {
+    console.log("AccordionBody rendering")
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
+
+export default UncontrolledAccordion
