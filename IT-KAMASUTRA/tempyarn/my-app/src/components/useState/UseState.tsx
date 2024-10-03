fb4a83b144b5b79cf71b16bcb24d6ae5b8f7c40b
zaytsev <zaicev@mniti.ru>
2024-10-03 14:13:59 +0300

    lesson 20 (useState) kabzda kak podrobno is Done

--- IT-KAMASUTRA/tempyarn/my-app/src/components/useState/UseState.tsx
+++ IT-KAMASUTRA/tempyarn/my-app/src/components/useState/UseState.tsx
@@ -6,11 +6,23 @@ function generateNumber() {
 
 export const UseState = () => {
     console.log("render")
+    //Передаем в качестве начального значения функцию. Реакт сделает это один раз.
+    // useMemo для этого не нужен
+    //1)
     const [counter, setCounter] = useState(generateNumber)
+
+    //2)setCounter вызывает функцию changer передавая в него старое значение и функция делает преобразования. Далее новое значение записывается в переменную counter
+    const changer = (state: number)=> {
+        return state+1
+    }
     return (
         <>
             <span>{counter}</span>
+            <br/>
             <button onClick={() => setCounter(counter + 1)}>+</button>
+            <br/>
+            <br/>
+            <button onClick={() => setCounter(changer)}>+</button>
         </>
     );
 };
